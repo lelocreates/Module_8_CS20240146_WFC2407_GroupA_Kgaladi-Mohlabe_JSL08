805d3782d8ae1bb7697c36f98d083a279172668a
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-09-22 17:04:14 +0200

    add class for BankBranch

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 // This one will be a little tricky. So check out this overview first: https://www.youtube.com/watch?v=sJ-c3BA-Ypo
 
 // 1. Create a variable to store the singleton instance of the bank branch. "bankBranchInstance"
-
+let bankBranchInstance = []; 
 // 2. Define a class called `BankBranch` for managing branch information.
-
+class BankBranch {
+    constructor(branchInfo){
+        if(bankBranchInstance.instance === null) {
+            this.branchInfo = branchInfo
+            bankBranchInstance.instance = this;
+        }else{
+            return bankBranchInstance;
+        }
+    }
+}
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
